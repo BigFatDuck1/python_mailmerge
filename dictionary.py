61b7d087e8a2c0ca60f834db19500e493e22d470
BigFatDuck1 <65705499+BigFatDuck1@users.noreply.github.com>
2021-08-28 17:59:20 +0800

    Revert "Initial commit"
    This reverts commit 23af3c3aaba91040c2a5043692c80949fad95e9b.

--- dictionary.py
+++
@@ -1,7 +0,0 @@
-#Contains names and their emails
-
-email_dict = {
-    "Olivia":"olivia.stone.olivia.stone@gmail.com",
-    "John":"a.nasty.bootlegger@gmail.com",
-    "Clement":"hoc1@connect.hku.hk",
-}
