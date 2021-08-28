23af3c3aaba91040c2a5043692c80949fad95e9b
BigFatDuck1 <65705499+BigFatDuck1@users.noreply.github.com>
2021-08-28 17:56:13 +0800

    Initial commit

---
+++ dictionary.py
@@ -0,0 +1,7 @@
+#Contains names and their emails
+
+email_dict = {
+    "Olivia":"olivia.stone.olivia.stone@gmail.com",
+    "John":"a.nasty.bootlegger@gmail.com",
+    "Clement":"hoc1@connect.hku.hk",
+}
---
+++ simple_test.py
@@ -0,0 +1,17 @@
+#Ignore this file 
+#You can use this to test if your email account is working
+#Did you disable enable less secure apps access?
+
+import smtplib
+
+#Sender email
+email = "example@gmail.com"
+#Sender password
+pw = "test123"
+
+server = smtplib.SMTP("smtp.gmail.com", 587)
+server.ehlo()
+server.starttls()
+server.login(email,pw)
+server.sendmail(email, email, msg="Subject: Hello there\n\n Hello there!")
+server.close()
